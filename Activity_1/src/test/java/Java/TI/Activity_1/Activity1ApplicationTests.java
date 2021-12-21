9a48f7e518e86a7c0f1c4868521c17b60591d5dc
ridwanam9 <94948013+ridwanam9@users.noreply.github.com>
2021-12-21 07:00:32 +0700

    Add files via upload

---
+++ Activity_1/src/test/java/Java/TI/Activity_1/Activity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Java.TI.Activity_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
